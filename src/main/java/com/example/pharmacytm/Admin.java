58c8906fe065e34ce357c0e6a88e0781dd581b42
manningtw <manningtw@oldcolony.info>
2023-11-30 12:59:58 -0500

    end of day - day 1

--- src/main/java/com/example/pharmacytm/Admin.java
+++ src/main/java/com/example/pharmacytm/Admin.java
@@ -2,8 +2,8 @@ package com.example.pharmacytm;
 
 public class Admin
 {
-    private int id;
-    private String name;
+    private final int id;
+    private final String name;
 
     public Admin(int id, String name)
     {
---
+++ src/main/java/com/example/pharmacytm/DButils.java
@@ -0,0 +1,9 @@
+package com.example.pharmacytm;
+
+public class DButils
+{
+    public void login()
+    {
+
+    }
+}
